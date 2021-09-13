45b473b2c45ae3c32fb961259af1d59eaa1302b4
Andrey Karaulov <aka@aiweapps.com>
2021-09-13 17:23:56 +0300

    fix bottom inset

--- Sources/Controllers/MessagesViewController+Keyboard.swift
+++ Sources/Controllers/MessagesViewController+Keyboard.swift
@@ -103,7 +103,7 @@ internal extension MessagesViewController {
 //         }
         
         if maintainPositionOnKeyboardFrameChanged && differenceOfBottomInset != 0 {
-//             let contentOffset = CGPoint(x: messagesCollectionView.contentOffset.x, y: messagesCollectionView.contentOffset.y + differenceOfBottomInset)
+            let contentOffset = CGPoint(x: messagesCollectionView.contentOffset.x, y: messagesCollectionView.contentOffset.y + differenceOfBottomInset)
 //             // Changing contentOffset to bigger number than the contentSize will result in a jump of content
 //             // https://github.com/MessageKit/MessageKit/issues/1486
 //             guard contentOffset.y <= messagesCollectionView.contentSize.height else {
